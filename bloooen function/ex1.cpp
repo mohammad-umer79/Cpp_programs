eebe1958881a9bbb8f6882af269a3873575a650a
Muhammad Umer <abbasihere864@gmail.com>
2025-07-03 06:49:26 -0700

    Day_03

---
+++ bloooen function/ex1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+bool isCodingFun = true;
+bool isFishTasty = false;
+
+    cout<<isFishTasty<<endl;
+    cout<< isCodingFun<<endl;
+return 0;
+}
